8f7c1c939be47c782338e49a911b1486c8299ce3
igor.fsantos18 <igor.fsantos18@VPR0576611W10-1>
2023-11-30 15:37:34 -0300

    Adicionar arquivos de projeto.

---
+++ Gerador de tabuadaa/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Gerador_de_tabuadaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdbUm = new System.Windows.Forms.RadioButton();
+            this.rdbDois = new System.Windows.Forms.RadioButton();
+            this.rdbTres = new System.Windows.Forms.RadioButton();
+            this.rdbQuatro = new System.Windows.Forms.RadioButton();
+            this.rdbCinco = new System.Windows.Forms.RadioButton();
+            this.rdbSeis = new System.Windows.Forms.RadioButton();
+            this.rdbSete = new System.Windows.Forms.RadioButton();
+            this.rdbOito = new System.Windows.Forms.RadioButton();
+            this.rdbNove = new System.Windows.Forms.RadioButton();
+            this.rdbDez = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.lstRes = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rdbUm
+            // 
+            this.rdbUm.AutoSize = true;
+            this.rdbUm.Location = new System.Drawing.Point(45, 127);
+            this.rdbUm.Name = "rdbUm";
+            this.rdbUm.Size = new System.Drawing.Size(31, 17);
+            this.rdbUm.TabIndex = 0;
+            this.rdbUm.TabStop = true;
+            this.rdbUm.Text = "1";
+            this.rdbUm.UseVisualStyleBackColor = true;
+            // 
+            // rdbDois
+            // 
+            this.rdbDois.AutoSize = true;
+            this.rdbDois.Location = new System.Drawing.Point(45, 150);
+            this.rdbDois.Name = "rdbDois";
+            this.rdbDois.Size = new System.Drawing.Size(31, 17);
+            this.rdbDois.TabIndex = 1;
+            this.rdbDois.TabStop = true;
+            this.rdbDois.Text = "2";
+            this.rdbDois.UseVisualStyleBackColor = true;
+            // 
+            // rdbTres
+            // 
+            this.rdbTres.AutoSize = true;
+            this.rdbTres.Location = new System.Drawing.Point(45, 173);
+            this.rdbTres.Name = "rdbTres";
+            this.rdbTres.Size = new System.Drawing.Size(31, 17);
+            this.rdbTres.TabIndex = 1;
+            this.rdbTres.TabStop = true;
+            this.rdbTres.Text = "3";
+            this.rdbTres.UseVisualStyleBackColor = true;
+            // 
+            // rdbQuatro
+            // 
+            this.rdbQuatro.AutoSize = true;
+            this.rdbQuatro.Location = new System.Drawing.Point(45, 196);
+            this.rdbQuatro.Name = "rdbQuatro";
+            this.rdbQuatro.Size = new System.Drawing.Size(31, 17);
+            this.rdbQuatro.TabIndex = 1;
+            this.rdbQuatro.TabStop = true;
+            this.rdbQuatro.Text = "4";
+            this.rdbQuatro.UseVisualStyleBackColor = true;
+            // 
+            // rdbCinco
+            // 
+            this.rdbCinco.AutoSize = true;
+            this.rdbCinco.Location = new System.Drawing.Point(45, 219);
+            this.rdbCinco.Name = "rdbCinco";
+            this.rdbCinco.Size = new System.Drawing.Size(31, 17);
+            this.rdbCinco.TabIndex = 1;
+            this.rdbCinco.TabStop = true;
+            this.rdbCinco.Text = "5";
+            this.rdbCinco.UseVisualStyleBackColor = true;
+            // 
+            // rdbSeis
+            // 
+            this.rdbSeis.AutoSize = true;
+            this.rdbSeis.Location = new System.Drawing.Point(45, 242);
+            this.rdbSeis.Name = "rdbSeis";
+            this.rdbSeis.Size = new System.Drawing.Size(31, 17);
+            this.rdbSeis.TabIndex = 1;
+            this.rdbSeis.TabStop = true;
+            this.rdbSeis.Text = "6";
+            this.rdbSeis.UseVisualStyleBackColor = true;
+            // 
+            // rdbSete
+            // 
+            this.rdbSete.AutoSize = true;
+            this.rdbSete.Location = new System.Drawing.Point(45, 265);
+            this.rdbSete.Name = "rdbSete";
+            this.rdbSete.Size = new System.Drawing.Size(31, 17);
+            this.rdbSete.TabIndex = 1;
+            this.rdbSete.TabStop = true;
+            this.rdbSete.Text = "7";
+            this.rdbSete.UseVisualStyleBackColor = true;
+            // 
+            // rdbOito
+            // 
+            this.rdbOito.AutoSize = true;
+            this.rdbOito.Location = new System.Drawing.Point(45, 288);
+            this.rdbOito.Name = "rdbOito";
+            this.rdbOito.Size = new System.Drawing.Size(31, 17);
+            this.rdbOito.TabIndex = 1;
+            this.rdbOito.TabStop = true;
+            this.rdbOito.Text = "8";
+            this.rdbOito.UseVisualStyleBackColor = true;
+            // 
+            // rdbNove
+            // 
+            this.rdbNove.AutoSize = true;
+            this.rdbNove.Location = new System.Drawing.Point(45, 311);
+            this.rdbNove.Name = "rdbNove";
+            this.rdbNove.Size = new System.Drawing.Size(31, 17);
+            this.rdbNove.TabIndex = 1;
+            this.rdbNove.TabStop = true;
+            this.rdbNove.Text = "9";
+            this.rdbNove.UseVisualStyleBackColor = true;
+            // 
+            // rdbDez
+            // 
+            this.rdbDez.AutoSize = true;
+            this.rdbDez.Location = new System.Drawing.Point(45, 334);
+            this.rdbDez.Name = "rdbDez";
+            this.rdbDez.Size = new System.Drawing.Size(37, 17);
+            this.rdbDez.TabIndex = 1;
+            this.rdbDez.TabStop = true;
+            this.rdbDez.Text = "10";
+            this.rdbDez.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(509, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "GERADOR DE TABUADA";
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.BackColor = System.Drawing.Color.Black;
+            this.btnGerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGerar.ForeColor = System.Drawing.Color.White;
+            this.btnGerar.Location = new System.Drawing.Point(12, 390);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(99, 50);
+            this.btnGerar.TabIndex = 4;
+            this.btnGerar.Text = "Gerar ";
+            this.btnGerar.UseVisualStyleBackColor = false;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // lstRes
+            // 
+            this.lstRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstRes.FormattingEnabled = true;
+            this.lstRes.ItemHeight = 31;
+            this.lstRes.Location = new System.Drawing.Point(240, 95);
+            this.lstRes.Name = "lstRes";
+            this.lstRes.Size = new System.Drawing.Size(260, 345);
+            this.lstRes.TabIndex = 5;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Black;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(117, 390);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(99, 50);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Limpar";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 525);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lstRes);
+            this.Controls.Add(this.btnGerar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rdbDez);
+            this.Controls.Add(this.rdbNove);
+            this.Controls.Add(this.rdbOito);
+            this.Controls.Add(this.rdbSete);
+            this.Controls.Add(this.rdbSeis);
+            this.Controls.Add(this.rdbCinco);
+            this.Controls.Add(this.rdbQuatro);
+            this.Controls.Add(this.rdbTres);
+            this.Controls.Add(this.rdbDois);
+            this.Controls.Add(this.rdbUm);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Gerador de tabuada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdbUm;
+        private System.Windows.Forms.RadioButton rdbDois;
+        private System.Windows.Forms.RadioButton rdbTres;
+        private System.Windows.Forms.RadioButton rdbQuatro;
+        private System.Windows.Forms.RadioButton rdbCinco;
+        private System.Windows.Forms.RadioButton rdbSeis;
+        private System.Windows.Forms.RadioButton rdbSete;
+        private System.Windows.Forms.RadioButton rdbOito;
+        private System.Windows.Forms.RadioButton rdbNove;
+        private System.Windows.Forms.RadioButton rdbDez;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.ListBox lstRes;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Gerador de tabuadaa/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gerador_de_tabuadaa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+  
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            if (rdbUm.Checked == true) {
+                lstRes.Items.Clear();
+                int num = 1;
+                for (int i = 0; i < 11; i++) {
+
+                   int res = num * i;
+                
+                    lstRes.Items.Add(num + " X " + i + " = " + res);
+                }
+
+               
+                
+             }
+
+            if (rdbDois.Checked == true)
+            {
+                   lstRes.Items.Clear();
+
+                int num = 2;
+                for (int i = 0; i < 11; i++)
+                {
+
+                    int res = num * i;
+
+                    lstRes.Items.Add(num + " X " + i + " = " + res);
+                }
+            }
+
+            if(rdbTres.Checked == true)
+            {
+                lstRes.Items.Clear();
+
+                int num = 3;
+                for (int i = 0; i < 11; i++)
+                {
+
+                    int res = num * i;
+
+                    lstRes.Items.Add(num + " X " + i + " = " + res);
+                }
+            }
+
+            if(rdbQuatro.Checked == true)
+            {
+                lstRes.Items.Clear();
+
+                int num = 4;
+                for (int i = 0; i < 11; i++)
+                {
+
+                    int res = num * i;
+
+                    lstRes.Items.Add(num + " X " + i + " = " + res);
+                }
+            }
+
+            if (rdbCinco.Checked == true)
+            {
+                lstRes.Items.Clear();
+
+                int num = 5;
+                for (int i = 0; i < 11; i++)
+                {
+
+                    int res = num * i;
+
+                    lstRes.Items.Add(num + " X " + i + " = " + res);
+                }
+            }
+
+            if (rdbSeis.Checked == true)
+            {
+                lstRes.Items.Clear();
+
+                int num = 6;
+                for (int i = 0; i < 11; i++)
+                {
+
+                    int res = num * i;
+
+                    lstRes.Items.Add(num + " X " + i + " = " + res);
+                }
+            }
+
+            if (rdbSete.Checked == true)
+            {
+                lstRes.Items.Clear();
+
+                int num = 7;
+                for (int i = 0; i < 11; i++)
+                {
+
+                    int res = num * i;
+
+                    lstRes.Items.Add(num + " X " + i + " = " + res);
+                }
+            }
+
+            if (rdbOito.Checked == true)
+            {
+                lstRes.Items.Clear();
+
+                int num = 8;
+                for (int i = 0; i < 11; i++)
+                {
+
+                    int res = num * i;
+
+                    lstRes.Items.Add(num + " X " + i + " = " + res);
+                }
+            }
+
+            if (rdbNove.Checked == true)
+            {
+                lstRes.Items.Clear();
+
+                int num = 9;
+                for (int i = 0; i < 11; i++)
+                {
+
+                    int res = num * i;
+
+                    lstRes.Items.Add(num + " X " + i + " = " + res);
+                }
+            }
+
+            if (rdbDez.Checked == true)
+            {
+                lstRes.Items.Clear();
+
+                int num = 10;
+                for (int i = 0; i < 11; i++)
+                {
+
+                    int res = num * i;
+
+                    lstRes.Items.Add(num + " X " + i + " = " + res);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstRes.Items.Clear();
+        }
+    }
+}
